da47b925fe20809c8e154c79848bb22d2f1b689a
PhantomOuO <wu0316aukj@gmail.com>
2022-12-23 11:45:17 +0800

    test:增加測試功能頁
    1.增加測試功能頁

---
+++ test.php
@@ -0,0 +1,51 @@
+<?php //2
+include("connMySQL.php");
+$sql_query = "SELECT * FROM language_type WHERE 1";
+$result = $db_link->query($sql_query);
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script>
+
+    </script>
+</head>
+
+<body>
+    <nav class="nav flex-column">
+        <?php 
+        
+        while($row_result = $result->fetch_assoc()){
+            echo "<a class='nav-link' href='' onkeydown='test($row_result[language_id])'>" . $row_result["name"] . "</a>";
+        }
+        $db_link->close(); 
+        function test($lang_id) {
+            echo "lang:".$lang_id;
+          }
+        ?>
+    </nav>
+
+    
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
