3e28147247d01b0d1f72be159d8e1863649ef517
PhantomOuO <wu0316aukj@gmail.com>
2023-01-13 21:57:47 +0800

    feat: 增加查詢會員編號
    1.增加查詢會員編號member_id
    2.Mag反饋訊息備註更新

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 /*
-Msg 0:登出 1:登入失敗 2:登入成功 3:帳號已註冊 4:註冊成功 5:帳號未註冊
+Msg 0:登出 1:登入失敗 2:登入成功 3:帳號已註冊 4:註冊成功 5:帳號未註冊 
+    6:發布文章成功 
 */
 
 //* 連結資料庫
@@ -14,13 +15,13 @@ if (isset($_SESSION["loginUserName"]) && ($_SESSION["loginUserName"] != "")) {
 //* 執行會員登錄
 if (isset($_POST["inputname"]) && isset($_POST["inputpasswd"])) {
     //* 聯繫登入會員資料 
-    $SQLQuery_RecLogin = "SELECT username, password FROM members WHERE username=?";
+    $SQLQuery_RecLogin = "SELECT member_id, username, password FROM members WHERE username=?";
     $result_RecLogin = $db_link->prepare($SQLQuery_RecLogin);
     $result_RecLogin->bind_param("s", $_POST["inputname"]);
     $result_RecLogin->execute();
 
     //* 取出帳號密碼的值 
-    $result_RecLogin->bind_result($username, $password);
+    $result_RecLogin->bind_result($member_id, $username, $password);
     $result_RecLogin->fetch();
     $result_RecLogin->close();
     //*檢查是否有資料
@@ -30,7 +31,9 @@ if (isset($_POST["inputname"]) && isset($_POST["inputpasswd"])) {
         //* 比對密碼， 若相同則登入成功
         if (password_verify($_POST["inputpasswd"],$password)) {
             //* 設定session
-            $_SESSION["loginUserName"] = "$username";
+            $_SESSION["loginUserName"] = $username;
+            $_SESSION["loginUserID"] = $member_id;
+
             header("Location: index.php?Msg=2");
             //*Cookie
             if (isset($_POST["rememberme"]) && ($_POST["rememberme"] == "true")) {
